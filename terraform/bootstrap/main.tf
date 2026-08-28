76ccd6c411d6e7dbe95efaae8afa1ad1e60bb7c9
Bruce Minanga <bruceminanga@gmail.com>
2026-08-28 21:53:50 +0300

    simple clear comments

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -16,6 +16,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# In modern Terraform, configuring an S3 bucket requires companion resources:
 # 1. State Bucket
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "my-company-prod-terraform-state-bucket-test2"
